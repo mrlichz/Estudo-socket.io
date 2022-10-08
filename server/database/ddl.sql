7b4a1a341ca629d161a400e3d99ff905db6cd153
mrlichz <thierryalefh77@gmail.com>
2022-10-08 13:06:05 -0300

    User table

--- server/database/ddl.sql
+++ server/database/ddl.sql
@@ -1,12 +1,31 @@
-create schema socketdb;
+create schema chatdb;
+create table tb_user (
+    id_user int primary key auto_increment,
+    nm_user varchar(30) not null,
+    ds_user varchar(300),
+    img_user varchar(300),
+    dt_created date default(curdate())
+);
+create table tb_user_login (
+    id_user_login int primary key auto_increment,
+    id_user int not null,
+    ds_email varchar(200) not null,
+    ds_password varchar(32) not null,
+    foreign key (id_user) references tb_user (id_user)
+);
 create table tb_channel (
     id_channel int unique primary key,
-    nm_channel varchar(20)
+    id_creator int not null,
+    nm_channel varchar(20),
+    dt_created date default(curdate()),
+    foreign key (id_creator) references tb_user (id_user)
 );
 create table tb_message (
     id_message int primary key auto_increment,
-    id_channel int,
-    ds_message varchar(500),
+    id_channel int not null,
+    id_user int not null,
+    ds_message varchar(500) not null,
     dt_sent date default(curdate()),
-    foreign key (id_channel) references tb_channel (id_channel)
+    foreign key (id_channel) references tb_channel (id_channel),
+    foreign key (id_user) references tb_user (id_user)
 );
--- server/database/dml.sql
+++ server/database/dml.sql
@@ -1,16 +1,29 @@
 use socketdb;
+-- Sign up
+insert into tb_user (nm_user)
+values('yolo');
+set @last = last_insert_id();
+insert into tb_user_login (id_user, ds_email, ds_password)
+values (@last, 'mrlichz0x01@hi.co', md5('hash'));
+-- Sign in
+select id_user id,
+    nm_user name,
+    ds_email email
+from tb_user_login
+    inner join tb_user on tb_user_login.id_user = tb_user.id_user
+where tb_user_login.ds_password = md5('hash');
 -- Create channel
-insert into tb_channel (id_channel, nm_channel)
-values (1, 'First');
+insert into tb_channel (id_channel, id_creator, nm_channel)
+values (1, 1, 'First');
 -- Create message
-insert into tb_message (ds_message, id_channel)
-values ('cool', 1);
+insert into tb_message (id_channel, id_user, ds_message)
+values (1, 1, 'cool');
 -- Get channel
 select id_channel
 from tb_channel
-where id_channel = ?;
+where id_channel = 1;
 --Get messages
 select id_message id,
     ds_message message
 from tb_message
-where id_channel = ?;
+where id_channel = 1;
